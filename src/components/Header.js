09c1b47e5604fef3adba00a78103d13c2b22006e
Trent Bullard <trent.bullard@onit.com>
2019-09-12 13:09:00 -0500

    add beta label to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => (
   <h1 className="ui center aligned header" style={{ paddingTop: "2em" }}>
     <Link to="/">Rank Tracker</Link>
+    <span className="ui red sub header">BETA</span>
   </h1>
 );
 
