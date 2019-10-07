5d02856c269eb1f880a87cc0d4882a642928bd84
Trent Bullard <trent.bullard@onit.com>
2019-10-07 10:22:31 -0500

    remove redux-related packages

--- src/index.js
+++ src/index.js
@@ -1,25 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import App from "./components/App";
-import reducers from "./reducers";
 import AuthProvider from "./contexts/AuthContext";
 import FlashProvider from "./contexts/FlashContext";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
-
 const Root = () => {
   return (
-    <Provider store={store}>
-      <FlashProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </FlashProvider>
-    </Provider>
+    <FlashProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </FlashProvider>
   );
 };
 
